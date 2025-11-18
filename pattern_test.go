ab99d1988d9d6a8515c0c80004b46b2f80deec58
Travis Keep <keep94@gmail.com>
2025-11-17 21:12:09 -0800

    Add defensive copy unit test for Pattern type.

--- pattern_test.go
+++ pattern_test.go
@@ -13,6 +13,15 @@ func TestIsZero(t *testing.T) {
 	assert.True(t, Ints().IsZero())
 }
 
+func TestDefensiveCopy(t *testing.T) {
+	digits := []int{1, 2, 3}
+	pattern := Ints(digits...)
+	forward := pattern.Forward()
+	copy(digits, []int{4, 5, 6})
+	copy(forward, []int{7, 8, 9})
+	assert.Equal(t, []int{1, 2, 3}, pattern.Forward())
+}
+
 func TestInts(t *testing.T) {
 	pattern := Ints(9, 0)
 	assert.Equal(t, []int{9, 0}, pattern.Forward())
